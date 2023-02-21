9bd2ff73623ecffff83b53a57ca178deb766e233
wmcharles <charleswafula73@gmail.com>
2023-02-21 09:48:00 +0300

    add config file

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php 
+
+// variables
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "contacts";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+?>
